ce9fb986d526fb3b90fb86cedd438a59859fd2e6
martinvrshkoski <mvrskoski@gmail.com>
2024-04-23 15:29:09 +0200

    Updated script.js
    Created functions

--- 05_JS_var_flode/Martin_Ovning/js/script.js
+++ 05_JS_var_flode/Martin_Ovning/js/script.js
@@ -18,12 +18,12 @@ console.log(currentDate);
 
 console.log(currentDate.getFullYear());
  */
-
+/* 
 console.log("--------------------------------");
 
 // Strängar
 
-/* console.log(stringNumber.toString()); */
+// console.log(stringNumber.toString());
 
 let stringNumber = 5;
 let convertNumber;
@@ -38,7 +38,7 @@ console.log(convertNumber);
 
 console.log("--------------------------------");
 
-/* fornamn.concat('', efternamn); */
+// fornamn.concat('', efternamn);
 
 console.log("HELLO".toLowerCase());
 
@@ -90,6 +90,88 @@ console.log("-------------------------------");
 let message = "Hej John";
 let newMessage = message.replace("John", "Johnny");
 console.log(newMessage);
+ */
 
+/* 
+console.log("-------------------------------");
+
+let txt1 = " Skärmskydd , 499";
+
+let txt2 = "Mobiltelefon, 299, ";
+
+let txt3 = " Telefonfodral, 89, ";
+
+let txt4 = " Laptop, 799";
+
+let txt5 = " Hörlurar , 199 ";
+
+// Dela upp sträng och extrahera varje produkt
+
+let txt1Product = txt1.substring(1, 12);
+let txt1Price = txt1.substring(13, 16);
+
+console.log("txt1Product: " +  txt1Product);
+console.log("txt1Price: " +   txt1Price);
+
+let txt1Split = txt1.split(",");
+console.log(txt1Split);
+
+for (let i = 0; i < txt1Split.length; i++) {
+    txt1Split[i] = txt1Split[i].trim();
+    
+}
+
+console.log(txt1Split);
+ */
+
+/* 
+console.log("--------------------------------");
+
+myName(); 
+saySomething("Hello World");
+
+
+function myName() {
+    let name = "Martin";
+    console.log(name);
+}
+
+function saySomething(txt) {
+    let newTxt = "User said that " + txt
+    console.log(newTxt);
+    return newTxt;
+}
+ */
 
 console.log("-------------------------------");
+
+
+
+
+writeName("Martin");
+console.log(area(10, 5)); 
+
+
+function writeName(name) {
+    console.log(name);
+}
+
+function area(width, height) {
+    let area = width * height
+    return area;
+}
+
+let var1 = "variable in ouer script";
+let var4;
+
+function func() {
+    let var2 = "variable in function scope";
+
+    if (true) {
+        let var3 = "variable in if statement scope";
+        var4 = "variable initialised in if statement scope";        
+        console.log(var1);
+    }
+}
+
+func();
