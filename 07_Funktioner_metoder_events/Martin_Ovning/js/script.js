1125706a1f2f8c366d98f23a921a5ea6c0d02264
martinvrshkoski <mvrskoski@gmail.com>
2024-04-24 11:59:47 +0200

    Update script.js

--- 07_Funktioner_metoder_events/Martin_Ovning/js/script.js
+++ 07_Funktioner_metoder_events/Martin_Ovning/js/script.js
@@ -1,33 +1,74 @@
-console.log("-------------------------------");
+/* console.log("-------------------------------");
 
-writeName("Martin");
-console.log(area(10, 5)); 
-
-
-function writeName(name) {
-    console.log(name);
-}
-
-function area(width, height) {
-    let area = width * height
-    return area;
-}
-
-let var1 = "variable in ouer script";
-let var4;
-
-function func() {
-    let var2 = "variable in function scope";
+// Function to add food dishes with counter++
+function addDish(dish, counter) {
+    if (counter[dish]) {
+        counter[dish]++;
+    } else {
+        counter[dish] = 1;
+    }
+    console.log(dish + " has been added to the list" + "\n");
     
-    if (true) {
-        let var3 = "variable in if statement scope";
-        var4 = "variable initialised in if statement scope";        
-        console.log(var1);
+}
+
+// Function to count the number of dishes
+function countDishes(counter) {
+    let totalCount = 0;
+    for (let dish in counter) {
+        totalCount += counter[dish];
+    }
+    return totalCount;
+}
+
+// Function to show added dishes
+function showAddedDishes(counter) {
+    console.log("Added Dishes:");
+    for (let dish in counter) {
+        console.log(`${dish}: ${counter[dish]}`);
     }
 }
 
-func();
+
+// Example usage
+let dishCounter = {};
+
+addDish("Pizza", dishCounter);
+addDish("Burger", dishCounter);
+addDish("Pizza", dishCounter);
+addDish("Sushi", dishCounter);
+addDish("Kebab", dishCounter);
+addDish("Kebab", dishCounter);
 
 
+console.log("Total number of dishes:", countDishes(dishCounter));
+
+showAddedDishes(dishCounter);
+ */
 
 console.log("-------------------------------");
+
+
+let counter = 0;
+
+function addDish() {
+    counter++;
+    console.log("Dish added");
+}
+
+function dishCounter() {
+    console.log(counter);
+}
+
+function showDishes() {
+    for (let i = 0; i < counter; i++) {
+        console.log(`${i} maträtt`);
+    }
+}
+
+addDish();
+addDish();
+addDish();
+addDish();
+dishCounter();
+addDish();
+showDishes();
